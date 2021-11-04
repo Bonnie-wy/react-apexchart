b7c152f30b7a4c9e9c031bd44a3eef8b6302ac14
Bonnie-wy <waiyeewong89@gmail.com>
2021-11-04 22:46:42 +0000

    onclick toggle function

--- src/components/PopChart.js
+++ src/components/PopChart.js
@@ -3,58 +3,66 @@ import Chart from 'react-apexcharts';
 
 const PopChart = () => {
 
- const [options, setOptions] = useState({
-   chart: {
-     background: '#f4f4f4',
-     foreColor: '#333'
-   },
-   xaxis: {
-     categories: ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Phoenix', 'San Antonio', 'San Diego', 'Dallas', 'San Jose']
-   },
-   plotOptions: {
-     bar: {
-       horizontal: false
-     }
-   },
-   fill: {
-     colors: ['#f44336']
-   },
-   dataLabels: {
-     enabled: false
-   },
-   title: {
-     text: 'Largest US Cities By Population',
-     align: 'center',
-     margin: 20,
-     offsetY: 20,
-     style: {
-       fontSize: '20px'
-     }
-   }
- })
+const [options, setOptions] = useState({
+  chart: {
+    background: '#f4f4f4',
+    foreColor: '#333'
+  },
+  xaxis: {
+    categories: ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Phoenix', 'San Antonio', 'San Diego', 'Dallas', 'San Jose']
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  fill: {
+    colors: ['#f44336']
+  },
+  dataLabels: {
+    enabled: false
+  },
+  title: {
+    text: 'Largest US Cities By Population',
+    align: 'center',
+    margin: 20,
+    offsetY: 20,
+    style: {
+      fontSize: '20px'
+    }
+  }
+})
 
- const [series, setSeries] = useState([{
-   name: 'Population',
-   data: [8550405, 3971883, 2720546, 2296224, 1567442, 1563025, 1469845, 1394928, 1300092, 1026908]
+const [series, setSeries] = useState([{
+  name: 'Population',
+  data: [8550405, 3971883, 2720546, 2296224, 1567442, 1563025, 1469845, 1394928, 1300092, 1026908]
+}])
 
- }])
+const handleClick = () => {
+  setOptions((prevState) => {
+    return ({
+      ...prevState,
+      plotOptions: {
+        bar: {
+          horizontal: !options.plotOptions.bar.horizontal
+        }
+      }
+    })
+  })
+}
 
- const handleClick = () => {
-   setOptions(!Object.keys(options.plotOptions.bar.horizontal))
- }
-
- return (
-   <>
-     <Chart
-       options={options}
-       series={series}
-       type="bar"
-       height="450"
-       width="100%"
-     />
-     <button onClick={handleClick}>Change</button>
-   </>
- )
+return (
+  <>
+    <Chart
+      options={options}
+      series={series}
+      type="bar"
+      height="450"
+      width="100%"
+    />
+    <button onClick={handleClick}>Change</button>
+  </>
+)
 }
 
 export default PopChart;
